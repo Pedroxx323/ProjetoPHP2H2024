344018d45314c0cc6ee5851d5f972927975abcdc
Pedroxx323 <pepefreitas2007@gmail.com>
2024-03-04 19:16:50 -0300

    exercicio 01 php

---
+++ exemplo01.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Exemplo01 - PHP</title>
+</head>
+<body>
+
+	<?php 
+	$a = 10;
+	echo "valor de a é " . $a;
+
+	 ?>
+
+</body>
+</html>
